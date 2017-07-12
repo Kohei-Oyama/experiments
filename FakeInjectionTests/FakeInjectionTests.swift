0075a42d78f0178f60fe4d2f984db649aa1d546d
Kohei-Oyama <fkgenhf108@ezweb.ne.jp>
2017-07-12 15:27:28 +0900

    Initial Commit

---
+++ FakeInjectionTests/FakeInjectionTests.swift
@@ -0,0 +1,36 @@
+//
+//  FakeInjectionTests.swift
+//  FakeInjectionTests
+//
+//  Created by Kohei Oyama on 2017/07/12.
+//  Copyright © 2017年 Kohei Oyama. All rights reserved.
+//
+
+import XCTest
+@testable import FakeInjection
+
+class FakeInjectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
